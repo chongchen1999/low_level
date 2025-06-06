728e7745d568ee46860e467f294d340b1c37be24
chongchen1999 <chongchen1999@gmail.com>
2025-06-06 11:33:05 +0800

    add unix related

---
+++ command_line/a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    
+    for (int i = n - 1; i >= 0; i--) {
+        std::cout << a[i] << ' ';
+    }
+    std::cout << std::endl;
+    return 0;
+
+}
