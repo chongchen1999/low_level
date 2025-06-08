8860593da63337e6b9777296bf0bc1c17517d8f2
chongchen1999 <chongchen1999@gmail.com>
2025-06-08 16:23:58 +0800

    update step 2 adding a library

--- cmake/code/Step2/tutorial.cxx
+++ cmake/code/Step2/tutorial.cxx
@@ -4,26 +4,26 @@
 #include <string>
 
 // TODO 5: Include MathFunctions.h
+#include "MathFunctions.h"
 #include "TutorialConfig.h"
 
-int main(int argc, char* argv[])
-{
-  if (argc < 2) {
-    // report version
-    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
-              << Tutorial_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        // report version
+        std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << "."
+                  << Tutorial_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+        return 1;
+    }
 
-  // convert input to double
-  double const inputValue = std::stod(argv[1]);
+    // convert input to double
+    double const inputValue = std::stod(argv[1]);
 
-  // TODO 6: Replace sqrt with mathfunctions::sqrt
-
-  // calculate square root
-  double const outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
-  return 0;
+    // TODO 6: Replace sqrt with mathfunctions::sqrt
+    // calculate square root
+    // double const outputValue = sqrt(inputValue);
+    double const outputValue = mathfunctions::sqrt(inputValue);
+    std::cout << "The square root of " << inputValue << " is " << outputValue
+              << std::endl;
+    return 0;
 }
